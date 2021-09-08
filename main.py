73a950029037de5d8c62831eda877ed598014449
Malkeir <badorr@wpi.edu>
2021-09-08 17:08:30 -0400

    lets go

---
+++ main.py
@@ -0,0 +1,42 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+
+
+
+
+
+
+
+
+class Terminal:
+
+    TERMINALCOMMANDS = {
+        "help":,"start #":(lambda x:exec( open(x,"r") )), "get":""}
+
+
+    def Terminal(self):
+        x = input("Màlkèir > ")
+        exec("print(hello)")
+
+
+
+    def getCommand(self):
+
+
+
+
+
+
+
+def main():
+
+    TRM = Terminal.Terminal()
+
+
+
+main()
